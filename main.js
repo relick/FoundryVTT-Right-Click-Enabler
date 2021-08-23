fc25ef350803409d006568bf3dab877fbc9fc61d
relick <drrintofen@gmail.com>
2021-08-23 19:41:02 +0100

    Reduce scope of Game class modification to just the activateListeners function.

--- main.js
+++ main.js
@@ -2,9 +2,9 @@ class RightClickEnabler
 {
 	static patchGame()
 	{
-		const classStr = Game.toString();
-		const patchedClassStr = classStr.replace(`document.addEventListener("contextmenu", ev => ev.preventDefault());`, ``);
-		Game = Function('"use strict"; return (' + patchedClassStr + ')')();
+		const fnStr = Game.prototype.activateListeners.toString();
+		const patchedFnStr = fnStr.replace(`document.addEventListener("contextmenu", ev => ev.preventDefault());`, ``);
+		Game.prototype.activateListeners = Function('"use strict"; return (function ' + patchedFnStr + ')')();
 	}
 
 	static ready()
