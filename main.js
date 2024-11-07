c048751ccf763ce222cb63e1e60235507a76407f
relick <relick@users.noreply.github.com>
2024-11-07 21:09:30 +0000

    Goodness me, I really messed up testing. restored code that I DID need

--- main.js
+++ main.js
@@ -6,7 +6,15 @@ class RightClickEnabler
 		const patchedFnStr = fnStr.replace(`document.addEventListener("contextmenu", ev => ev.preventDefault());`, ``);
 		Game.prototype.activateListeners = Function('"use strict"; return (function ' + patchedFnStr + ')')();
 	}
+
+	static ready()
+	{
+		// Canvas doesn't handle global right-click being re-enabled properly (still true as of V12)
+		const canvas = document.getElementById("board");
+		canvas.addEventListener("contextmenu", (ev) => ev.preventDefault(), { capture: true });
+	}
 };
 
 // The Game class has to be patched pre-init
 RightClickEnabler.patchGame();
+Hooks.once("ready", RightClickEnabler.ready);
